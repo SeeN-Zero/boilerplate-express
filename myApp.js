2878a6b1b4bb90314e88a8e552970e6e86b79274
Senna Annaba Ahmad <71185988+SeeN-Zero@users.noreply.github.com>
2022-03-06 16:21:53 +0700

    fix

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var express = require("express");
 var app = express();
 
@@ -10,9 +11,9 @@ app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/json", (req, res) => {
   if (process.env.MESSAGE_STYLE === "uppercase") {
-    res.json({ message: "HELLO JSON" });
+    res.json({"message": "HELLO JSON" });
   } else {
-    res.json({ message: "Hello json" });
+    res.json({ "message": "Hello json" });
   }
 });
 
